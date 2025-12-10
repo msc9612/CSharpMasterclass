c39595f7c13d5cc93d4859f40f71fe038e405ac6
Matthew Chambers <msc9612@gmail.com>
2025-12-09 22:13:59 -0500

    Second git

---
+++ ImplicitVariables.cs
@@ -0,0 +1,13 @@
+//implicitely typed variables
+
+var myFavoriteGenre = "Science Fiction";
+var myLuckyNumber = 7;
+var pi = 3.14159;
+var isRaining = false;
+var numbers = new[] { 1, 2, 3, 4, 5 };
+
+//Referency type
+//explicitly typed variable
+string myName = "Matthew";
+int myAge = 29;
+
