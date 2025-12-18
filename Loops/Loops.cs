48763288d8a1f67548b04449ebb549ba35f3c45f
Matthew <73714913+msc9612@users.noreply.github.com>
2025-12-18 15:43:11 -0500

    Add files via upload

---
+++ Loops/Loops.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//for loops
+//count up
+for (int i=0; i<=10; i++)
+{
+    Console.WriteLine($"I={i}");
+}
+
+Console.ReadKey();
+//count down
+for (int i=10; i>=0; i--)
+{
+    Console.WriteLine($"I={i}");
+
+}
+
+Console.ReadKey();
+
+//while loops
+
+//do-while loops
+//foreach loops
+
---
+++ RocketLandingSimulation/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+var Rocket = @"
+            ^
+           / \
+           | |
+           | |
+           | |
+          /___\
+            W";
+string space = "\n";
+
+for (int i = 10; i >= 0; i--)
+{
+
+    Console.WriteLine(String.Concat(Enumerable.Repeat(space, 10-i)));
+    Console.WriteLine(Rocket);
+    Console.WriteLine(String.Concat(Enumerable.Repeat(space, i)));
+    Console.WriteLine("________________________________________");
+    Thread.Sleep(1000);
+    Console.Clear();
+
+}
+
+Console.WriteLine("You have reached the moon!");
+Console.WriteLine(String.Concat(Enumerable.Repeat(space, 10)));
+Console.WriteLine(Rocket);
+Console.WriteLine("________________________________________");
+Thread.Sleep(6000);
+Console.Clear();
